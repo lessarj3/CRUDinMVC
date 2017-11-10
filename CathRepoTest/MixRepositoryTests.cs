3ae437ae5035920021caa4f450022e86adfccad7
lemm0038 <jeff.lemmerman@gmail.com>
2017-11-10 16:21:11 -0600

    Added Test class for Mix validation Added Test for adding new Mix

--- CathRepoTest/MixRepositoryTests.cs
+++ CathRepoTest/MixRepositoryTests.cs
@@ -9,11 +9,21 @@ namespace CathRepoTest
     public class MixRepositoryTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGettingMixes()
         {
             var repo = MixRepositoryFactory.Get();
             var mixes = repo.GetMixes();
             Assert.AreEqual(2, mixes.ToList().Count());
+        }
+
+        [TestMethod]
+        public void TestAddingNewMix()
+        {
+            var repo = MixRepositoryFactory.Get();
+            var mixes = repo.GetMixes();
+            Assert.AreEqual(2, mixes.ToList().Count());
+            repo.AddMix(new Mix { Id = 1, MixName = "Mix 1", CFx = 12, SVO = 24, Carbon = 32, Binder = 23, Ratio = 23 });
+            Assert.AreEqual(3, mixes.ToList().Count());
         }
     }
 }
---
+++ CathRepoTest/MixValidationTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CathRepoCommon.Models;
+using System.Linq;
+
+namespace CathRepoTest
+{
+    [TestClass]
+    public class MixValidationTests
+    {
+        [TestMethod]
+        public void TestMixNameTooLong()
+        {
+            var mix = new Mix();
+            mix.MixName = "VeryLongMixNamemmmmmmmmmmmmmmm";
+            mix.Ratio = 1;
+            var results = ValidateModel(mix);
+            Assert.AreEqual(1, results.Count());
+            Assert.AreEqual(results[0].ErrorMessage, "Mix Name too long");
+        }
+
+
+        private List<ValidationResult> ValidateModel<T>(T model)
+        {
+            var context = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+            var valid = Validator.TryValidateObject(model, context, result, true);
+            return result;
+        }
+    }
+}
